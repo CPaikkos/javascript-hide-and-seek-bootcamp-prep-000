f785ced41f9b42e4e6a4816822b091f5a90f1565
Caroline Paikkos <carolinefprice@gmail.com>
2017-05-06 14:22:16 +0000

    Done.

--- index.js
+++ index.js
@@ -7,8 +7,22 @@ function nestedTarget() {
 }
 
 function increaseRankBy(n) {
-  var rankedList = document.querySelectorAll('.ranked-list li');
+  var rankedList = document.getElementById('app').querySelectorAll('.ranked-list li');
   for(var i = 0; i < rankedList.length; i++) {
-    rankedList[i].innerHTML = parseInt(i) + n;
+
+     rankedList[i].innerHTML = parseInt(rankedList[i].innerHTML) + n;
   }
+}
+
+function deepestChild() {
+  //return document.querySelector('#grand-node div div div div'); passes
+  //return document.getElementById('#grand-node').children; doesn't pass
+  //return document.querySelector('#grand-node *'); passes
+      var children = document.getElementById('grand-node');
+      var nextChild = children.children[0];
+  while (nextChild) {
+     children = nextChild;
+     nextChild = children.children[0];
+}
+return children;
 }
